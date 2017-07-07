26444a11f30167cac33ebf89b479bd0dc4e0b950
Qalbinuril Setyani <qalbins@gmail.com>
2017-07-07 08:14:44 +0700

    update maps

---
+++ app/src/main/java/com/example/amrizalns/botic/activity/directionActivity.java
@@ -0,0 +1,54 @@
+package com.example.amrizalns.botic.activity;
+
+import android.location.LocationListener;
+import android.support.v4.app.FragmentActivity;
+import android.os.Bundle;
+
+import com.example.amrizalns.botic.R;
+import com.google.android.gms.maps.CameraUpdateFactory;
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.OnMapReadyCallback;
+import com.google.android.gms.maps.SupportMapFragment;
+import com.google.android.gms.maps.model.BitmapDescriptorFactory;
+import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.MarkerOptions;
+
+import java.util.ArrayList;
+
+public class directionActivity extends FragmentActivity implements OnMapReadyCallback {
+
+    private GoogleMap mGoogleMap;
+    SupportMapFragment mMapFragment;
+    ArrayList<LatLng> mMarkerPoint;
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_direction);
+        mMapFragment = (SupportMapFragment) getSupportFragmentManager()
+                .findFragmentById(R.id.map);
+        mMapFragment.getMapAsync(this);
+    }
+
+    @Override
+    public void onMapReady(GoogleMap googleMap) {
+        mGoogleMap = googleMap;
+
+        LatLng sydney = new LatLng(-34, 151);
+        mGoogleMap.addMarker(new MarkerOptions().position(sydney).title("yeay"));
+        mGoogleMap.moveCamera(CameraUpdateFactory.newLatLng(sydney));
+    }
+
+    private void drawMarker(LatLng point) {
+        mMarkerPoint.add(point);
+
+        MarkerOptions options = new MarkerOptions();
+        if (mMarkerPoint.size() == 1) {
+            options.icon(BitmapDescriptorFactory.defaultMarker(BitmapDescriptorFactory.HUE_BLUE));
+        } else if (mMarkerPoint.size() == 2) {
+            options.icon(BitmapDescriptorFactory.defaultMarker(BitmapDescriptorFactory.HUE_RED));
+        }
+        mGoogleMap.addMarker(options);
+    }
+}

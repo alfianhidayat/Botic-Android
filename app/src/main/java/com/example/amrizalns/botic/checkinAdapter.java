ddb5c0359f96e98c408069a4365fd4ca3587c30b
Qalbinuril Setyani <qalbins@gmail.com>
2017-07-12 11:27:40 +0700

    update checkin

---
+++ app/src/main/java/com/example/amrizalns/botic/checkinAdapter.java
@@ -0,0 +1,71 @@
+package com.example.amrizalns.botic;
+
+import android.content.Context;
+import android.support.v7.widget.RecyclerView;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+import com.example.amrizalns.botic.model.CheckinData;
+
+import java.util.List;
+
+/**
+ * Created by user on 12/07/2017.
+ */
+
+public class checkinAdapter extends RecyclerView.Adapter<checkinAdapter.MyViewHolder> {
+    private List<CheckinData> mCheckinDatas;
+    private int rowLayout;
+    private Context mContext;
+    private ItemClickListener clickListener;
+
+    public class MyViewHolder extends RecyclerView.ViewHolder implements View.OnClickListener {
+        public TextView nama, asal;
+
+        public MyViewHolder(View view) {
+            super(view);
+            nama = (TextView) view.findViewById(R.id.txt_nama_checkin);
+            asal = (TextView) view.findViewById(R.id.txt_asal_checkin);
+            view.setTag(view);
+            view.setOnClickListener(this);
+        }
+
+        @Override
+        public void onClick(View view) {
+            if (clickListener != null) clickListener.onClick(view, getAdapterPosition());
+        }
+    }
+
+    public checkinAdapter(List<CheckinData> checkinDatas, int rowLayout, Context context) {
+        this.mCheckinDatas = checkinDatas;
+        this.rowLayout =rowLayout;
+        this.mContext = context;
+    }
+
+
+    @Override
+    public checkinAdapter.MyViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
+        View itemView = LayoutInflater.from(parent.getContext())
+                .inflate(R.layout.checkindata_list_row, parent, false);
+
+        return new MyViewHolder(itemView);
+    }
+
+    @Override
+    public void onBindViewHolder(MyViewHolder holder, int position) {
+        CheckinData checkinData = mCheckinDatas.get(position);
+        holder.nama.setText(checkinData.getNama());
+        holder.asal.setText(checkinData.getAsal());
+    }
+
+    @Override
+    public int getItemCount() {
+        return mCheckinDatas.size();
+    }
+
+    public void setClickListener(ItemClickListener itemClickListener) {
+        this.clickListener = itemClickListener;
+    }
+}
--- app/src/main/java/com/example/amrizalns/botic/fragment/checkin_2.java
+++ app/src/main/java/com/example/amrizalns/botic/fragment/checkin_2.java
@@ -3,13 +3,18 @@ package com.example.amrizalns.botic.fragment;
 import android.app.FragmentTransaction;
 import android.app.ProgressDialog;
 import android.content.Context;
+import android.content.DialogInterface;
 import android.content.Intent;
 import android.graphics.Color;
 import android.net.Uri;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
 import android.support.v4.content.IntentCompat;
+import android.support.v7.app.AlertDialog;
+import android.support.v7.widget.DefaultItemAnimator;
 import android.support.v7.widget.LinearLayoutCompat;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -21,32 +26,33 @@ import android.widget.Toast;
 
 import com.botic.coreapps.callbacks.PageCallback;
 import com.botic.coreapps.networks.RetrofitApi;
+import com.example.amrizalns.botic.AktivitasAdapter;
+import com.example.amrizalns.botic.ItemClickListener;
 import com.example.amrizalns.botic.R;
 import com.example.amrizalns.botic.activity.booking_gedung;
 import com.example.amrizalns.botic.activity.mainInterface;
 import com.example.amrizalns.botic.activity.signIn;
+import com.example.amrizalns.botic.checkinAdapter;
+import com.example.amrizalns.botic.model.CheckinData;
 import com.example.amrizalns.botic.utils.SessionLogin;
 
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.Collections;
+import java.util.List;
 
-public class checkin_2 extends Fragment implements View.OnClickListener{
+
+public class checkin_2 extends Fragment implements View.OnClickListener, ItemClickListener{
 
     View view;
     private LinearLayout mLinearLayout;
     Button btnCheckInSubmit;
-    //    private EditText nama, usia, asal;
-//    private TextView nmr;
     private int i = 0;
     private ProgressDialog dialog;
-
-    public checkin_2() {
-        // Required empty public constructor
-    }
-
-    public static checkin_2 newInstance(String param1, String param2) {
-        checkin_2 fragment = new checkin_2();
-
-        return fragment;
-    }
+    private List<CheckinData> mCheckinDataList =  new ArrayList<>();
+    private RecyclerView recyclerView;
+    private checkinAdapter mCheckinAdapter;
+    String nama, asal;
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
@@ -54,66 +60,35 @@ public class checkin_2 extends Fragment implements View.OnClickListener{
     }
 
     @Override
-    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+    public View onCreateView(final LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         view = inflater.inflate(R.layout.fragment_checkin_2, container, false);
 
-        mLinearLayout = (LinearLayout) view.findViewById(R.id.checkin_tamu_form);
+        recyclerView = (RecyclerView) view.findViewById(R.id.rv_checkin);
+
+        mCheckinAdapter = new checkinAdapter(mCheckinDataList, R.layout.checkindata_list_row, getContext());
+        RecyclerView.LayoutManager mLayoutManager = new LinearLayoutManager(getContext());
+        recyclerView.setLayoutManager(mLayoutManager);
+        recyclerView.setItemAnimator(new DefaultItemAnimator());
+        recyclerView.setAdapter(mCheckinAdapter);
+
         btnCheckInSubmit = (Button) view.findViewById(R.id.btn_checkin_tamu);
         btnCheckInSubmit.setOnClickListener(this);
+        mCheckinAdapter.setClickListener(this);
+
         String jumlah = getArguments().getString("daftar");
         int jmlTamu = Integer.parseInt(jumlah);
-        for (i = 0; i < jmlTamu; i++) {
-            FormTamu();
+        for (int i = 0; i < jmlTamu; i++) {
+            CheckinData checkinData = new CheckinData((i+1)+". Isi Data Pengunjung "+ (i+1), asal);
+            mCheckinDataList.add(checkinData);
         }
+        mCheckinAdapter.notifyDataSetChanged();
+
         dialog= new ProgressDialog(getActivity());
         dialog.setMessage("Loading...");
         dialog.setCancelable(false);
         return view;
     }
-
-    private void FormTamu() {
-        TextView nomer = new TextView(getActivity());
-        nomer.setId(nomer.generateViewId());
-        nomer.setText("Tamu " + (i + 1));
-        nomer.setTextSize(14);
-        nomer.setTextColor(getResources().getColor(R.color.colorContent));
-        nomer.setLayoutParams(new LinearLayout.LayoutParams(LinearLayout.LayoutParams.MATCH_PARENT, LinearLayout.LayoutParams.WRAP_CONTENT));
-        mLinearLayout.addView(nomer);
-
-        EditText nama = new EditText(getActivity());
-        nama.setId(nama.generateViewId());
-        nama.setHint("nama");
-        nama.setTextSize(14);
-        nama.setTextColor(getResources().getColor(R.color.colorContent));
-        nama.setHintTextColor(getResources().getColor(R.color.colorContent));
-        nama.setLayoutParams(new LinearLayout.LayoutParams(LinearLayout.LayoutParams.MATCH_PARENT, LinearLayout.LayoutParams.WRAP_CONTENT));
-        mLinearLayout.addView(nama);
-
-        EditText usia = new EditText(getActivity());
-        usia.setId(usia.generateViewId());
-        usia.setHint("usia");
-        usia.setTextSize(14);
-        usia.setTextColor(getResources().getColor(R.color.colorContent));
-        usia.setHintTextColor(getResources().getColor(R.color.colorContent));
-        usia.setLayoutParams(new LinearLayout.LayoutParams(LinearLayout.LayoutParams.MATCH_PARENT, LinearLayout.LayoutParams.WRAP_CONTENT));
-        mLinearLayout.addView(usia);
-
-        EditText asal = new EditText(getActivity());
-        asal.setId(asal.generateViewId());
-        asal.setHint("asal pengunjung");
-        asal.setTextSize(14);
-        asal.setTextColor(getResources().getColor(R.color.colorContent));
-        asal.setHintTextColor(getResources().getColor(R.color.colorContent));
-        asal.setLayoutParams(new LinearLayout.LayoutParams(LinearLayout.LayoutParams.MATCH_PARENT, LinearLayout.LayoutParams.WRAP_CONTENT));
-        mLinearLayout.addView(asal);
-
-        View view = new View(getContext());
-        view.setLayoutParams(new LinearLayoutCompat.LayoutParams(LinearLayoutCompat.LayoutParams.MATCH_PARENT, 3));
-        view.setBackgroundColor(Color.parseColor("#000000"));
-        mLinearLayout.addView(view);
-    }
-
 
     @Override
     public void onClick(View v) {
@@ -157,5 +132,38 @@ public class checkin_2 extends Fragment implements View.OnClickListener{
                         });
                 break;
         }
+    }
+
+    @Override
+    public void onClick(View view, final int position) {
+        final CheckinData checkinData = mCheckinDataList.get(position);
+        LayoutInflater layoutInflaterAndroid = LayoutInflater.from(getContext());
+        View mView = layoutInflaterAndroid.inflate(R.layout.input_data_tamu, null);
+        AlertDialog.Builder alertDialogBuilderUserInput = new AlertDialog.Builder(getContext());
+        alertDialogBuilderUserInput.setView(mView);
+        final EditText namaInputDialogEditText = (EditText) mView.findViewById(R.id.namaInputDialog);
+        final EditText asalInputDialogEditText = (EditText) mView.findViewById(R.id.asalInputDialog);
+        EditText usiaInputDialogEditText = (EditText) mView.findViewById(R.id.usiaInputDialog);
+        alertDialogBuilderUserInput
+                .setCancelable(false)
+                .setPositiveButton("OK", new DialogInterface.OnClickListener() {
+                    public void onClick(DialogInterface dialogBox, int id) {
+                        nama = namaInputDialogEditText.getText().toString();
+                        asal = asalInputDialogEditText.getText().toString();
+                        checkinData.setNama(nama);
+                        checkinData.setAsal(asal);
+                        mCheckinAdapter.notifyDataSetChanged();
+                    }
+                })
+                .setNegativeButton("Batal",
+                        new DialogInterface.OnClickListener() {
+                            public void onClick(DialogInterface dialogBox, int id) {
+                                dialogBox.cancel();
+                            }
+                        });
+
+        AlertDialog alertDialogAndroid = alertDialogBuilderUserInput.create();
+        alertDialogAndroid.show();
+        mCheckinAdapter.notifyDataSetChanged();
     }
 }
